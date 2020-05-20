98554f2f0144a79ab1ba9d1100d6936f8f415d38
Ry Sutton <ryancesutton@gmail.com>
2020-05-20 12:44:19 -0500

    progress before lunch

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,16 +1,19 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class Student {
 
-    private static long id;
-    private static String name;
-    private static ArrayList<Integer> grades;
+    public static long id;
+    public static String name;
+    public static ArrayList<Integer> grades;
 
     public Student (long id, String name, ArrayList<Integer> grades){
         this.id = id;
         this.name = name;
         this.grades = grades;
     }
+
+
+
+
 
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,4 +1,26 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class StudentTest {
 
+    @Test
+    public void testIfGetIdWorks () {
+        assertEquals(7777777, Student.id);
+    }
+
+    @Test
+    public void testIfGetNameWorks () {
+        assertEquals("Ry Sutton", Student.name);
+    }
+
+    @Test
+    public void testIfAddGradeWorks () {
+        assertEquals(99, Student.grades.lastIndexOf(99));
+    }
+
+    @Test
+    public void testIfGetAverageWorks () {
+
+    }
 
 }
