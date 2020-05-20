597c56545191253beb39fa32eb2e0446a3b6fa2c
Ry Sutton <ryancesutton@gmail.com>
2020-05-20 16:11:09 -0500

    make corrections during walkthrough

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,19 +1,40 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Student {
 
-    public static long id;
-    public static String name;
-    public static ArrayList<Integer> grades;
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
 
-    public Student (long id, String name, ArrayList<Integer> grades){
+    public Student (long id, String name){
         this.id = id;
         this.name = name;
-        this.grades = grades;
+        this.grades = new ArrayList<>();
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList<Integer> getGrades () {
+        return this.grades;
     }
 
 
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
 
-
-
+    public double getAverage() {
+        double sum = 0;
+        for (Integer grade: grades) {
+            sum += grade;
+        }
+        return sum / grades.size();
+    }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,26 +1,48 @@
+import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class StudentTest {
 
+    Student ry;
+
+    @Before
+    public void setUp(){
+        ry = new Student (7777777, "Ry Sutton");
+    }
+
+
+
     @Test
     public void testIfGetIdWorks () {
-        assertEquals(7777777, Student.id);
+        assertEquals(7777777, ry.getId());
     }
 
     @Test
     public void testIfGetNameWorks () {
-        assertEquals("Ry Sutton", Student.name);
+        assertEquals("Ry Sutton", ry.getName());
+    }
+
+    @Test
+    public void testIfGetGradeWorks () {
+        assertEquals(0, ry.getGrades().size());
     }
 
     @Test
     public void testIfAddGradeWorks () {
-        assertEquals(99, Student.grades.lastIndexOf(99));
+        ry.addGrade(100);
+        ry.getGrades().get(0);
+        assertTrue(100 ==  ry.getGrades().get(0));
+        ry.addGrade(80);
+        assertTrue(100 ==  ry.getGrades().get(0));
     }
 
     @Test
     public void testIfGetAverageWorks () {
-
+        ry.addGrade(100);
+        ry.addGrade(80);
+        assertEquals(90, ry.getAverage(), 0.1);
     }
 
 }
